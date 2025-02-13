a2097e5a6e681b50c89a5bc9d6d9b489271cbbf1
EA1 - Samuel Oliveira Da Silva <silva_samuel02@educadventista.org>
2025-02-13 20:27:03 -0300

    aula1

---
+++ oi .py
@@ -0,0 +1,7 @@
+Gasto1 = float(input("Cartão de Credito: "))
+Gasto2 = float(input("Cartão de Debito: "))
+Gasto3 = float(input("Boleto: "))
+
+ValorTotal = Gasto1 + Gasto2 + Gasto3
+
+print(f"Valor Total de Conta: {ValorTotal}")
